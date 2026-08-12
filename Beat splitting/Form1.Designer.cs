9ba6416f5d0190e38a84e6f5401d1aa0726aac56
joyeongjun <sksk47389@naver.com>
2026-08-12 10:56:25 +0900

    Add files via upload

---
+++ Beat splitting/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(131, 148);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 23);
+            button1.TabIndex = 0;
+            button1.Text = "음악열기버튼";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(393, 149);
+            button2.Name = "button2";
+            button2.Size = new Size(174, 22);
+            button2.TabIndex = 1;
+            button2.Text = "음악열기버튼";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Location = new Point(309, 267);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 100);
+            panel1.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(61, 61);
+            button5.Name = "button5";
+            button5.Size = new Size(113, 23);
+            button5.TabIndex = 0;
+            button5.Text = "현위치";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(61, 32);
+            button4.Name = "button4";
+            button4.Size = new Size(113, 23);
+            button4.TabIndex = 0;
+            button4.Text = "-";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(61, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(113, 23);
+            button3.TabIndex = 0;
+            button3.Text = "+";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+    }
+}
