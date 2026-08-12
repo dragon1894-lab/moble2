5cd0741d8abbcf02b2ba2b6b77cac96530e08718
ChoMinKyeong13 <minko1113@naver.com>
2026-08-12 14:40:56 +0900

    로그인/회원가입 페이지 및 DB 연동 기능 추가

---
+++ login1_0811/DBHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace login1_0811
+{
+    internal class DBHelper
+    {
+        private static string connStr = 
+            "Server=localhost;Database=djing;Uid=root;Pwd=1111;";
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            return conn;
+        }
+    }
+}
