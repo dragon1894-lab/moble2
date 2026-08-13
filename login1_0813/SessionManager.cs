543cc1ad75863a0940df2a9723064cb819fa8d5d
dragon1894-lab <dragon1894@gmail.com>
2026-08-13 17:15:25 +0900

    Add login1_0813 SessionManager.cs

---
+++ login1_0813/SessionManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace login1_0811
+{
+    public static class SessionManager
+    {
+        public static string CurrentUserId { get; set; }
+        public static string CurrentUserName { get; set; }
+
+        public static bool IsLoggedIn()
+        {
+            return !string.IsNullOrEmpty(CurrentUserId);
+        }
+
+    }
+}
